d18dc42f7902e3f141b6340b7315a226032110c3
Felix <felix.plagge@gmail.com>
2020-07-25 23:44:25 +0200

    renaming

--- tests/test_performance_graph.py
+++ tests/test_performance_graph.py
@@ -3,12 +3,12 @@ import os
 
 def test_toplvl():
     n_files_before = len(os.listdir("./tests"))
-    os.system("performance_graph.py tests/script_test_case_1.py")
+    os.system("performance_graph tests/script_test_case_1.py")
     n_files_after = len(os.listdir("./tests"))
     assert n_files_before == n_files_after - 1, "No png file output."
 
 def test_lowlvl():
     n_files_before = len(os.listdir("./tests/sub_dir"))
-    os.system("performance_graph.py tests/sub_dir/script_test_case_2.py")
+    os.system("performance_graph tests/sub_dir/script_test_case_2.py")
     n_files_after = len(os.listdir("./tests/sub_dir"))
     assert n_files_before == n_files_after - 1, "No png file output."
